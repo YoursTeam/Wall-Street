b4cbcd082c0fa5861f593d87396a767c90977e17
陈 <cxz_13290613756@163.com>
2020-12-06 20:55:43 +0800

    12.6

--- js/cxz/gyhejyy.js
+++ js/cxz/gyhejyy.js
@@ -1,1 +1,16 @@
 $('.config_header').load('/html/config/config.html')
+$('.next').on('click',()=>{
+	// console.log(1)
+	$('#quanqiuBox_ul').animate({left:'0'})
+})
+$('.prop').on('click',()=>{
+	// console.log(1)
+	$('#quanqiuBox_ul').animate({left:'-75%'})
+})
+$('.zuixinBox_swiperBox_next').on('click',()=>{
+	$('.yidongBox').attr('style','transform:translateX(0)')
+})
+$('.zuixinBox_swiperBox_prop').on('click',()=>{
+	$('.yidongBox').attr('style','transform:translateX(-100%)')
+
+})
