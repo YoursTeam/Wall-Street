88d70cc6748291a79816a4cf67d2b3978474d66e
陈 <cxz_13290613756@163.com>
2020-12-09 19:13:43 +0800

    12.9修改

--- js/qrf/course.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$(".nav-tabs li").click(function(){
-		$(".product-list__content .tab-pane").eq($(this).index()).addClass("cur").siblings().removeClass('cur');
-		$(".nav-tabs li").eq($(this).index()).addClass("tabs_color").siblings().removeClass('tabs_color');
-		
-	})
-	$(".product-list__content .tab-pane").eq(0).addClass("cur").siblings().removeClass('cur');
-})
